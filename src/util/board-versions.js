bf32ee0b8018a73dd99b6e85c2db190326657788
Bence Frenyó <26139379+bencefr@users.noreply.github.com>
2019-08-09 12:26:41 +0200

    Add boardversion support for Thingy:91

--- src/util/board-versions.js
+++ src/util/board-versions.js
@@ -9,6 +9,9 @@ const BoardVersion = {
 };
 
 const getBoardVersion = serialNumber => {
+    if (serialNumber.toString().startsWith('PCA')) {
+        return serialNumber.toString().split('_')[0];
+    }
     const sn = parseInt(serialNumber, 10).toString();
     const digits = sn.substring(0, 3);
     return BoardVersion[digits];
